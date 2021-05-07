15c0ed13bc2397620a4b45ab52e8af637ce3a874
Elias Ramirez <ramirezelias110@gmail.com>
2021-05-07 15:35:27 -0400

    userorders.js

--- src/componet/userorders.js
+++ src/componet/userorders.js
@@ -1,10 +1,37 @@
-const UserOrders = () =>{
+import axios from 'axios'
+import React, { useEffect } from 'react'
+
+
+
+const UserOrders = () => {
 
     return(
 
-        <div>
-            hello from Orders
-        </div>
+    <div>
+        <h1>Orders</h1>
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th>ORDER ID</th>
+                        <th>USER</th>
+                        <th>DATE</th>
+                        <th>TOTAL</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {orders.map((order) => (
+                        <tr key={order._id}>
+                            <td>{order._id}</td>
+                            <td>{order.user.name}</td>
+                            <td>{order.createdAt.substring(0, 10)}</td>
+                            <td>{order.totalPrice.toFixed(2)}</td>
+                        
+                            
+                        </tr>
+                    ))}
+                </tbody>
+            </table>        
+    </div>
     )
 
 }
